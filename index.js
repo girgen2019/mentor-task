8d41aea0ac93570b6b635619e1aba03a536729d3
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2021-07-26 14:22:55 +0300

    Task 1-4: create new task

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-// =============================== Задание 1  =============================================
+// =================================== Задание 1 ===========================================
 
 const sweets = [
-    { id: 1, price: 20, name: 'chocolate' },
-    { id: 2, price: 10, name: 'ice-cream' },
-    { id: 3, price: 30, name: 'coca-cola' },
+  { id: 1, price: 20, name: 'chocolate' },
+  { id: 2, price: 10, name: 'ice-cream' },
+  { id: 3, price: 30, name: 'coca-cola' },
 ]
-    let answerOnQuestion = prompt('Что вы хотите покушать?')
-    let searchChocolate = sweets.find(names => names.name === answerOnQuestion)
-    if (searchChocolate) {
-        alert(`Товар ${searchChocolate.name} стоит ${searchChocolate.price} рублей`)
-    } else {
-        alert('Товар не был найден')
-    }
-    let searchIceCream = sweets.find(names => names.name === answerOnQuestion)
-    if (searchIceCream) {
-        alert(`Товар ${searchIceCream.name} стоит ${searchIceCream.price} рублей`)
-    } else {
-        alert('Товар не был найден')
-   }
-    let searchCocaCola = sweets.find(names => names.name === answerOnQuestion)
-    if (searchCocaCola) {
-        alert(`Товар ${searchCocaCola.name} стоит ${searchCocaCola.price} рублей`)
-    } else {
-        alert('Товар не был найден')
-   }
+let answerOnQuestion = prompt('Что вы хотите покушать?')
+let searchChocolate = sweets.find(names => names.name === answerOnQuestion)
+if (searchChocolate) {
+  alert(`Товар ${searchChocolate.name} стоит ${searchChocolate.price} рублей`)
+} else {
+  alert('Товар не был найден')
+}
+let searchIceCream = sweets.find(names => names.name === answerOnQuestion)
+if (searchIceCream) {
+  alert(`Товар ${searchIceCream.name} стоит ${searchIceCream.price} рублей`)
+} else {
+  alert('Товар не был найден')
+}
+let searchCocaCola = sweets.find(names => names.name === answerOnQuestion)
+if (searchCocaCola) {
+  alert(`Товар ${searchCocaCola.name} стоит ${searchCocaCola.price} рублей`)
+} else {
+  alert('Товар не был найден')
+}
 
 // // ==================   Задание 2   ======================================
 
-    let enterCommand = prompt('Введите комманду!')
-    if(enterCommand === 'All'){
+let enterCommand = prompt('Введите комманду!')
+if (enterCommand === 'All') {
   sweets.forEach(all => alert(`Товар ${all.name} стоит ${all.price}`))
 }
-    else if(enterCommand === 'Low price'){
-        sweets.filter((element) => {
+else if (enterCommand === 'Low price') {
+  sweets.filter((element) => {
     return element.price < 15;
-}).forEach(lows => alert(`Самый дешевый товар - это ${lows.name} стоит ${lows.price}`))
+  }).forEach(lows => alert(`Самый дешевый товар - это ${lows.name} стоит ${lows.price}`))
 
 
 }
-else if(enterCommand === 'High price'){
- sweets.filter((element) => {
+else if (enterCommand === 'High price') {
+  sweets.filter((element) => {
     return element.price > 15;
-}).forEach(highs => alert(`Самый дорогой товар - это ${highs.name} стоит ${highs.price}`))
+  }).forEach(highs => alert(`Самый дорогой товар - это ${highs.name} стоит ${highs.price}`))
 } else {
-alert('Товар не найден')
+  alert('Товар не найден')
 }
 
 
@@ -54,9 +54,22 @@ const sweets = [
   { id: 2, price: 10, name: 'ice-cream' },
   { id: 3, price: 30, name: 'coca-cola' },
 ]
-let newSweets = sweets.map(discount => `Цены со скидкой 20% - товар ${discount.name} по цене - ${Math.round(discount.price * 100 / 120)}`) 
+let newSweets = sweets.map(discount => `Цены со скидкой 20% - товар ${discount.name} по цене - ${Math.round(discount.price * 100 / 120)}`)
 console.log(newSweets)
 
 
+// ============================   Задание 4  =======================================================
 
 
+const sweets = [
+  { id: 1, price: 20, name: 'chocolate' },
+  { id: 2, price: 10, name: 'ice-cream' },
+  { id: 3, price: 30, name: 'coca-cola' },
+]
+function giveDiscount() {
+  let discounts = prompt('Введиде скидку %')
+  let result = sweets.map(discount => `${discount.price}` - (`${discount.price}` * `${discounts / 100}`))
+  return `Товар со скидкой стоит  ${result}`
+}
+
+alert(giveDiscount())
