781d94a360d8628aebf3fb7c43754bb32fb71d51
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2021-08-03 18:00:03 +0300

    Fixing bugs in task 1, 2, 4

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
 // =================================== Задание 1 ===========================================
 
-// const sweets = [
-//   { id: 1, price: 20, name: 'chocolate' },
-//   { id: 2, price: 10, name: 'ice-cream' },
-//   { id: 3, price: 30, name: 'coca-cola' },
-// ]
-// let answerOnQuestion = prompt('Что вы хотите покушать?')
-// let searchProduct = sweets.find(names => names.name === answerOnQuestion)
-// if (searchProduct) {
-//   alert(`Товар ${searchChocolate.name} стоит ${searchChocolate.price} рублей`)
-// } else {
-//   alert('Товар не был найден')
-// }
+ const sweets = [
+  { id: 1, price: 20, name: 'chocolate' },
+  { id: 2, price: 10, name: 'ice-cream' },
+  { id: 3, price: 30, name: 'coca-cola' },
+]
+ const answerOnQuestion = prompt('Что вы хотите покушать?')
+ const searchProduct = sweets.find(nameProduct => nameProduct.name === answerOnQuestion)
+ if (searchProduct) {
+   alert(`Товар ${searchProduct.name} стоит ${searchProduct.price} рублей`)
+ } else {
+   alert(`Товара с именем "${answerOnQuestion}" не было найдено. Попробуйте выбрать что-то другое.`)
+ }
 
 // // // ==================   Задание 2   ======================================
 
-// let enterCommand = prompt('Введите комманду!')
-// if (enterCommand === 'All') {
-//   sweets.forEach(all => alert(`Товар ${all.name} стоит ${all.price}`))
-// }
-// else if (enterCommand === 'Low price') {
-//   sweets.filter((element) => {
-//     return element.price < 15;
-//   }).forEach(lows => alert(`Самый дешевый товар - это ${lows.name} стоит ${lows.price}`))
-// }
-// else if (enterCommand === 'High price') {
-//   sweets.filter((element) => {
-//     return element.price > 15;
-//   }).forEach(highs => alert(`Самый дорогой товар - это ${highs.name} стоит ${highs.price}`))
-// } else if (enterCommand === 'All low price'){
-//   let isAllLowPrice = true
-//   sweets.forEach((sweet) => {
-//     if(sweet.price > 15 ){
-//       isAllLowPrice = false
-//     }
-//   })
-//   if(isAllLowPrice){
-//     alert(`Все товары являются категорией Low Price`)
-//   }
-//   else{
-//     alert('НЕ Все товары являются категорией Low Price')
-//   }
-// sweets.filter((element) => {
-// element.price > 0 < 15 })
-
-//  return 
-// }
-// else {
-//   alert('Товар не найден')
-// }
+const enterCommand = prompt('Введите комманду!')
+if (enterCommand === 'All') {
+  sweets.forEach(all => alert(`Товар ${all.name} стоит ${all.price}`))
+}
+else if (enterCommand === 'Low price') {
+  sweets.filter((element) => {
+    return element.price < 15;
+  }).forEach(low => alert(`Самый дешевый товар - это ${low.name} стоит ${low.price}`))
+}
+else if (enterCommand === 'High price') {
+  sweets.filter((element) => {
+    return element.price > 15;
+  }).forEach(high => alert(`Самый дорогой товар - это ${high.name} стоит ${high.price}`))
+} else if (enterCommand === 'All low price'){
+  let isAllLowPrice = true
+  sweets.forEach((sweet) => {
+    if(sweet.price > 15 ){
+      isAllLowPrice = false
+    }
+  })
+  if(isAllLowPrice){
+    alert(`Все товары являются категорией Low Price`)
+  }
+  else{
+    alert('НЕ Все товары являются категорией Low Price')
+  }
+sweets.filter((element) => {
+element.price > 0 < 15 }) 
+}
+else {
+  alert('Товар не найден')
+}
 // // ==========================  Задание 3     ==================================================
 
 
-// const sweets = [
-//   { id: 1, price: 20, name: 'chocolate' },
-//   { id: 2, price: 10, name: 'ice-cream' },
-//   { id: 3, price: 30, name: 'coca-cola' },
-// ]
-// // map
+const sweets = [
+  { id: 1, price: 20, name: 'chocolate' },
+  { id: 2, price: 10, name: 'ice-cream' },
+  { id: 3, price: 30, name: 'coca-cola' },
+]
+// map
 
-// const newSweets = sweets.map((discount) => {
-//  return `id: 1, price: ${Math.round(discount.price * 100 / 120)}, name: ${discount.name}`
-// })
-// console.log(newSweets)
+const newSweets = sweets.map((discount) => {
+ return `id: 1, price: ${Math.round(discount.price * 100 / 120)}, name: ${discount.name}`
+})
+console.log(newSweets)
 
-// // цикл for
+// цикл for
 
-// for(i = 0; i < sweets.length; i++){
-//   sweets[i].price =  Math.round(sweets[i].price * 100 /120)
-//   console.log(sweets[i])
-// }
+for(i = 0; i < sweets.length; i++){
+  sweets[i].price =  Math.round(sweets[i].price * 100 /120)
+  console.log(sweets[i])
+}
 
 
 // ============================   Задание 4  =======================================================
@@ -80,46 +78,40 @@ const sweets = [
   { id: 2, price: 10, name: 'ice-cream' },
   { id: 3, price: 30, name: 'coca-cola' },
 ]
-const giveDiscount = function(){
-
-}
-
-const giveDiscount = () => {
-
-}
-
 function giveDiscount(sweetsArray) {
   const discounts = prompt('Введиде скидку %')
-  // const discounts = 10
 
-  // Цикл for
+  // вариант решения с помощью цикла for
   // if (Number(discounts)) {
   //   for (i = 0; i < sweets.length; i++) {
   //     sweets[i].price = Math.round((sweets[i].price) - (sweets[i].price * `${discounts}`/ 100 ))
   //     console.log(sweets[i])
 
-  // map
+  // вариант решения с помощью метода map
   if (Number(discounts)) {
 
     const newSweetsArray = sweetsArray.map((sweet) => {
       sweet.price = (sweet.price) - (sweet.price * `${discounts}` / 100)
       return sweet
     })
-    // for (let i = 0; i < newSweetsArray.length; i++) {
-    //   alert(`Продукт ${newSweetsArray[i].name} стоит со скидкой ${newSweetsArray[i].price} рублей`)
-    // }
-    newSweetsArray.forEach((sweet, sweetIndex) => {
-      alert(`Продукт ${sweet.name} стоит со скидкой ${sweet.price} рублей`)
-      alert(sweetIndex)
-    })
-    let newSweetsLowPrice = newSweetsArray.filter((sweet) => {
-      return sweet.price > 8
-    })
-    newSweetsLowPrice.forEach((sweet) =>{
-      alert(`${sweet.name}`)
-    })
-    
-    return newSweetsArray
+    for (let i = 0; i < newSweetsArray.length; i++) {
+      alert(`Продукт ${newSweetsArray[i].name} стоит со скидкой ${newSweetsArray[i].price} рублей`)
+    }
+
+    //=== начало тренировочного блока кода ======
+    // newSweetsArray.forEach((sweet, sweetIndex) => {
+    //   alert(`Продукт ${sweet.name} стоит со скидкой ${sweet.price} рублей`)
+    //   alert(sweetIndex)
+    // })
+    // let newSweetsLowPrice = newSweetsArray.filter((sweet) => {
+    //   return sweet.price > 8
+    // })
+    // newSweetsLowPrice.forEach((sweet) =>{
+    //   alert(`${sweet.name}`)
+    // })
+
+    // ======конец тренировочного блока кода =======
+        return newSweetsArray
   }
   else {
     console.log('Неправильный формат скидки')
@@ -131,47 +123,48 @@ console.log(giveDiscount(sweets))
 
 //=======================  Задание 5   =================================
 
-// const numbers1 = [14, 1234, -5, 100];
-// const numbers2 = [234, 12, 1234, 5, 23];
-// const numbers3 = [-1, -4, 0, 1, 2, 5, -100];
+const numbers1 = [14, 1234, -5, 100];
+const numbers2 = [234, 12, 1234, 5, 23];
+const numbers3 = [-1, -4, 0, 1, 2, 5, -100];
 
-// spread
+spread
 
-//  function findMinimalNumbers(numbers){
-//  return Math.min(...numbers)
-//  }
+ function findMinimalNumbers(numbers){
+ return Math.min(...numbers)
+ }
 
-//  const findMinimalNumbers = function (numbers) {
-//   return Math.min(...numbers)
-//  }
+ const findMinimalNumbers = function (numbers) {
+  return Math.min(...numbers)
+ }
 
-//  const findMinimalNumbers = ((numbers) => {return Math.min(...numbers)})
+ const findMinimalNumbers = ((numbers) => {return Math.min(...numbers)})
 
-// function findMinimalNumbers(numbers){
+function findMinimalNumbers(numbers){
 
-//   for(i = 0; i < numbers.length; i++){
-//    return (Math.min(...numbers))
-//   }
-//   return 
-//    }
+  for(i = 0; i < numbers.length; i++){
+   return (Math.min(...numbers))
+  }
+  return 
+   }
 
-// console.log(findMinimalNumbers(numbers1))
-// console.log(findMinimalNumbers(numbers2))
-// console.log(findMinimalNumbers(numbers3))
+console.log(findMinimalNumbers(numbers1))
+console.log(findMinimalNumbers(numbers2))
+console.log(findMinimalNumbers(numbers3))
 
 //========================= Задание 6  =====================================
 
-// const matrix = [
-//   [14, 1234, -5, 100],
-//   [234, 12, 1234, 5, 23],
-//   [-1, -4, 0, 1, 2, 15, -100],
-//  ];
+const matrix = [
+  [14, 1234, -5, 100],
+  [234, 12, 1234, 5, 23],
+  [-1, -4, 0, 1, 2, 15, -100],
+ ];
  
-// for(let i = 0; i < matrix.length; i++){
-//   for(let y = 0; y < matrix[i].length; y++){
-//     console.log(i,y)
-// }
-// }
+for(let i = 0; i < matrix.length; i++){
+  console.log([i])
+  for(let y = 0; y < matrix[i].length; y++){
+    console.log(i,y)
+}
+}
 
 // ====================================  Задание 7   ===========================================
 
--- index1.js
+++ index1.js
@@ -63,7 +63,7 @@
 // // const findMinimalNumbers = ((numbers) => {return Math.min(...numbers)})
 
 // function findMinimalNumbers(numbers){
-  
+
 //   for(i = 0; i < numbers.length; i++){
 //    return (Math.min(...numbers))
 //   }
@@ -74,47 +74,136 @@
 // console.log(findMinimalNumbers(numbers2))
 // console.log(findMinimalNumbers(numbers3))
 
-const matrix = [
-  [14, 1234, -5, 100],
-  [234, 12, 1234, 5, 23],
-  [-1, -4, 0, 1, 2, 15, -100],
- ];
- 
- const matrix2 = [
-  [75, 45, 2, -5, 12],
-  [43, 100, 101, 102, 0, 0.5],
-  [6, 1, 234, 50, 21],
- ]
+// const matrix = [
+//   [14, 1234, -5, 100],
+//   [234, 12, 1234, 5, 23],
+//   [-1, -4, 0, 1, 2, 15, -100],
+//  ];
 
- const matrix3 = [
-  [75, 45, 2, -5555, 12],
-  [43, 100, 101, 102, 0, 0.5],
-  [6, -1, 234, 50, 21],
- ] 
- function getSumOfLowNumbersInMatrix (min){
-  for(let i = 0; i < min.length; i++){ 
-    console.log(Math.min.apply(null, min[i]))
-}
-return
- }
-  getSumOfLowNumbersInMatrix(matrix3)
+//  const matrix2 = [
+//   [75, 45, 2, -5, 12],
+//   [43, 100, 101, 102, 0, 0.5],
+//   [6, 1, 234, 50, 21],
+//  ]
 
-// function getMax(a){
-//   return Math.max(...a.map(e => Array.isArray(e) ? getMax(e) : e));
+//  const matrix3 = [
+//   [75, 45, 2, -5555, 12],
+//   [43, 100, 101, 102, 0, 0.5],
+//   [6, -1, 234, 50, 21],
+//  ] 
+//  function getSumOfLowNumbersInMatrix (min){
+//   for(let i = 0; i < min.length; i++){ 
+//     console.log(Math.min.apply(null, min[i]))
+// }
+// return
+//  }
+//   getSumOfLowNumbersInMatrix(matrix2)
+
+// // function getMax(a){
+// //   return Math.max(...a.map(e => Array.isArray(e) ? getMax(e) : e));
+// // }
+
+// var arr = [[1, 37.8, 80.8, 41.8],
+// [2, 30.9, 69.5, 32.4],
+// [3, 25.4, 57, 25.7],
+// [4, 11.7, 18.8, 10.5],
+// [5, 11.9, 17.6, 10.4],
+// [6, 8.8, 13.6, 7.7],
+// [7, 7.6, 12.3, 9.6],
+// [8, 12.3, 29.2, 10.6],
+// [9, 16.9, 42.9, 14.8],
+// [10, 12.8, 30.9, 11.6],
+// [11, 5.3, [6.1, [56.7, [98.55]]], 4.7],
+// [12, 6.6, 8.4, 5.2],
+// [13, 4.8, 6.3, 3.6],
+// [14, 4.2, 6.2, 3.4]];
+
+// function maxValueArray(maxValue) {
+//   for (let i = 0; i < maxValue.length; i++){
+//       console.log(Math.max.apply(null, maxValue[[[i]]]));
+//     }
+
+//   return ;
 // }
 
-// var arr = [[1,  37.8, 80.8, 41.8],
-//            [2,  30.9, 69.5, 32.4],
-//            [3,  25.4,   57, 25.7],
-//            [4,  11.7, 18.8, 10.5],
-//            [5,  11.9, 17.6, 10.4],
-//            [6,   8.8, 13.6,  7.7],
-//            [7,   7.6, 12.3,  9.6],
-//            [8,  12.3, 29.2, 10.6],
-//            [9,  16.9, 42.9, 14.8],
-//            [10, 12.8, 30.9, 11.6],
-//            [11,  5.3,  [6.1,[56.7,[98.55]]],  4.7],
-//            [12,  6.6,  8.4,  5.2],
-//            [13,  4.8,  6.3,  3.6],
-//            [14,  4.2,  6.2,  3.4]];
-//  console.log(getMax(arr));
+// maxValueArray(arr)
+
+let objFamily = {
+  // firstNameOne: 'Gena',
+  // lastNameOne: 'Girilovich',
+  // firstNameTwo: 'Ekaterina',
+  // lastNameTwo: 'Bokuts',
+}
+
+
+// for( let prop in objFamily){
+//   console.log(prop)
+// }
+
+// objFamily.status = 'We are living'
+
+//  console.log(objFamily)
+
+// function isEmpty(obj){
+// for(let prop in obj){
+//   return console.log(true)
+// }
+//   return console.log(false)
+// }
+//  isEmpty(objFamily)
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// let sum = 0
+// for(let prop in salaries){
+//   sum += salaries[prop]
+// } 
+// console.log(sum)
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+let menu1 = {
+  glemglmeo: 200,
+  egoneigne: 300,
+  eogeogn: "My menu"
+};
+let newMenu = {}
+Object.assign(newMenu,menu,menu1)
+console.log(newMenu)
+
+// let clonMenu = {}
+
+// for( let prop in menu){
+//   clonMenu[prop] = menu[prop]
+// }
+
+// console.log(clonMenu)
+
+// let newProp = 0
+// function multiplyNumeric(obj){
+//   for(let prop in obj) {
+//     if(typeof menu[prop] === Number){
+//       newProp = menu[prop] * 2 
+//     } else if (typeof menu[prop] === String){
+//       newProp = menu[prop]
+//     }
+//   }
+//   return
+// }
+// function multiplyNumeric(menu){
+//   for (let prop in menu) {
+//     if (typeof menu[prop] === "number") {
+//       console.log(newProp = menu[prop] * 2)
+//   } else if (typeof menu[prop] === "string") {
+//     console.log(newProp = menu[prop])
+//   }
+//   }
+// }
+
+// multiplyNumeric(menu)
