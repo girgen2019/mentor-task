2e2930b0880df26f283fd64c647a75db80ddc263
Hirylovich Henadzi <70638680+girgen2019@users.noreply.github.com>
2021-08-10 17:30:37 +0300

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,31 @@
+let anyNumber = 100000
+anyNumber = '10000'
+console.log(100000, anyNumber, typeof anyNumber)
+
+let anyString = 'Gena'
+anyString = true
+console.log('Gena', anyString, typeof anyString)
+
+let anyBooleanTrue = true
+let anyBooleanFalse = false
+anyBooleanTrue = 'true'
+anyBooleanFalse = "false"
+console.log(true, anyBooleanTrue, typeof anyBooleanTrue)
+console.log(false, anyBooleanFalse, typeof anyBooleanFalse)
+
+let anyBigInt = 10000n
+anyBigInt = true
+console.log(10000n, anyBigInt, typeof anyBigInt)
+
+let anyNull = null
+anyNull = undefined
+console.log(null, anyNull, typeof anyNull)
+
+let anyUndefined = undefined
+console.log(undefined, anyUndefined, typeof anyUndefined)
+
+let anyObject = "Object"
+console.log(Object, anyObject, typeof anyObject)
+
+let anySymbol = "Symbol"
+console.log(Symbol, anySymbol, typeof anySymbol)
