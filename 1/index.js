59421d9a4199ff1aff98b4949a4dd25586b3d30a
Hirylovich Henadzi <70638680+girgen2019@users.noreply.github.com>
2021-08-10 17:28:51 +0300

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,30 @@
+
+// ========== Задание 1 =====================
+// const anyNumber = 100000
+// console.log(100000, anyNumber, typeof anyNumber)
+
+// const anyString = 'Gena'
+// console.log('Gena', anyString, typeof anyString)
+
+// const anyBooleanTrue = true
+// const anyBooleanFalse = false
+// console.log(true, anyBooleanTrue, typeof anyBooleanTrue)
+// console.log(false, anyBooleanFalse, typeof anyBooleanFalse)
+
+// const anyBigInt = 10000n
+// console.log(10000n, anyBigInt, typeof anyBigInt)
+
+// const anyNull = null
+// console.log(null, anyNull, typeof anyNull)
+
+// const anyUndefined = undefined
+// console.log(undefined, anyUndefined, typeof anyUndefined)
+
+// const anyObject = Object
+// console.log(Object, anyObject, typeof anyObject)
+
+// const anySymbol = Symbol
+// console.log(Symbol, anySymbol, typeof anySymbol)
+
+//==================== Задание 2 =======================
+
