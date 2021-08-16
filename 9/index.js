99163cd4040b4426c6e0e718ac3c77c8d351d723
Hirylovich Henadzi <70638680+girgen2019@users.noreply.github.com>
2021-08-16 13:19:06 +0300

    Add files via upload

--- 9/index.js
+++ 9/index.js
@@ -1,8 +1,26 @@
-function getSumInRage(from, to){
+// function getSumInRage(from = 0, to = 50){
+//     let sumInterval = 0
+//     for( from = 0; from <= to; from++){
+//         sumInterval+=from
+//     }
+// return sumInterval
+// }
+// console.log(getSumInRage(0,50))
+
+// const getSumInRage = (from = 0, to = 50) => {
+//     let sumInterval = 0
+//     for (from = 0; from <= to; from++) {
+//         sumInterval += from
+//     }
+//     return sumInterval
+// }
+// console.log(getSumInRage())
+
+const getSumInRage = function(from = 0, to = 50){
     let sumInterval = 0
     for( from = 0; from <= to; from++){
         sumInterval+=from
     }
 return sumInterval
 }
-console.log(getSumInRage(1,6))
+console.log(getSumInRage())
